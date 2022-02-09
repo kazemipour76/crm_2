4572c61ac90db9d783e83cec6d8788738413e884
kazemipour76 <ali.kazemipour@yahoo.com>
2022-02-09 15:15:55 +0330

    add function error for show error

--- resources/views/components/backend/full-text-search.blade.php
+++ resources/views/components/backend/full-text-search.blade.php
@@ -5,9 +5,13 @@
     ])
 
 <div class="input-group mb-3">
-    <input type="text" class="form-control" name="{{ $name }}"  value="{{ $value }}" form="{{ $formId }}" placeholder="جستجو در لیست"
+    <input type="text" {{$attributes->class([ 'form-control' , 'is-invalid' => $errors->has($name) ])}} name="{{ $name }}"  value="{{ $value }}" form="{{ $formId }}" placeholder="جستجو در لیست"
            aria-describedby="basic-addon2"  style="border-radius: 0 !important">
     <div class="input-group-append">
         <button class="btn btn-primary" type="submit" form="{{ $formId }}" style="border-radius: 0 !important"> <x-backend.icon class="fa-search"/></button>
     </div>
+
+@error($name)
+<div class="invalid-feedback">{{$message}}</div>
+@enderror
 </div>

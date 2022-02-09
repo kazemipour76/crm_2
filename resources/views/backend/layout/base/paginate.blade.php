ca6a997b8666477edec95b9a225e096840dd96ba
kazemipour76 <ali.kazemipour@yahoo.com>
2022-02-09 16:29:14 +0330

    add features for keep value query

--- resources/views/backend/layout/base/paginate.blade.php
+++ resources/views/backend/layout/base/paginate.blade.php
@@ -2,12 +2,14 @@
     <div class="d-flex justify-content-between align-items-center flex-wrap">
         {{--        <a href="#" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i class="ki ki-bold-double-arrow-next icon-xs"></i></a>--}}
         @if (!$paginator->onFirstPage())
-            <a href="{{ $paginator->url(1) }}" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i
+            <a href="{{ $paginator->appends(request()->query())->url(1) }}" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i
                     class="ki ki-bold-double-arrow-next icon-xs"></i></a>
-            <a href="{{ $paginator->previousPageUrl() }}" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i
+            <a href="{{ $paginator->appends(request()->query())->previousPageUrl() }}" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i
                     class="ki ki-bold-arrow-next icon-xs"></i></a>
         @endif
-        @foreach ($elements as $element)
+
+
+    @foreach ($elements as $element)
             @if (is_string($element))
                 <a href="#"
                    class="btn btn-icon btn-sm border-0 btn-light btn-hover-primary active mr-2 my-1">{{ $element }}</a>
@@ -18,18 +20,19 @@
                         <a href="#"
                            class="btn btn-icon btn-sm border-0 btn-light btn-hover-primary active mr-2 my-1">{{ $page }}</a>
                     @else
-                        <a href="{{ $url }}"
+                        <a href="{{ $paginator->appends(request()->query())->url($page)  }}"
                            class="btn btn-icon btn-sm border-0 btn-light mr-2 my-1">{{ $page }}</a>
                     @endif
                 @endforeach
             @endif
         @endforeach
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i
+            <a href="{{ $paginator->appends(request()->query())->nextPageUrl() }}" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i
                     class="ki ki-bold-arrow-back icon-xs"></i></a>
-            <a href="{{ $paginator->url($paginator->lastPage()) }}" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i
+            <a href="{{ $paginator->url($paginator->appends(request()->query())->lastPage()) }}" class="btn btn-icon btn-sm btn-light mr-2 my-1"><i
                     class="ki ki-bold-double-arrow-back icon-xs"></i></a>
         @endif
     </div>
+
 @endif
 

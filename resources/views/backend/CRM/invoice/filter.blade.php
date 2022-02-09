3e5ab1f69c744b5371b26b997c1db67a4921236a
kazemipour76 <ali.kazemipour@yahoo.com>
2022-02-09 17:41:59 +0330

    add new features for search box

--- resources/views/backend/CRM/invoice/filter.blade.php
+++ resources/views/backend/CRM/invoice/filter.blade.php
@@ -2,17 +2,45 @@
     <x-backend.card icon="fa-filter" title="جست و جوی پیشرفته"
                     color="6"
                     icon="fa-filter"
-                    :isCollapse="!\App\Utilities\Request::hasQuery()">
+{{--                    :isCollapse="!\App\Utilities\Request::hasQuery()"--}}
+    >
         <div class="col-12">
             <div class="card-body">
                 <div class="row">
+                    <div class="col-lg-6 mb-3">
+                        <x-backend.form.form-group title="جست و جو بر اساس نام مشتری">
+                            <x-backend.form.select2 name="customer">
+                                <option value="0" @if(empty(old('customer'))) selected @endif>لطفا انتخاب کنید</option>
+                                @foreach(\App\Models\CRM\Customer::all() as $customer)
+                                    <option value="{{$customer->id}}"
+                                            @if(old('customer') === "$customer->id") selected @endif>{{$customer->name}}</option>
+                                @endforeach
+                            </x-backend.form.select2>
+                        </x-backend.form.form-group>
+                    </div>
+                    <div class="col-lg-6">
+                        <x-backend.form.form-group title="جست و جو بر اساس شماره پیش فاکتور">
+                            <x-backend.form.input name="perInvoiceNumber" :value="old('perInvoiceNumber')"/>
+                        </x-backend.form.form-group>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <x-backend.form.form-group title="جست و جو بر اساس عنوان  پیش فاکتور">
+                            <x-backend.form.input name="title" :value="old('perInvoiceTitle')"/>
+                        </x-backend.form.form-group>
+                    </div>
+                    <div class="col-lg-6">
+                        <x-backend.form.form-group title="جست و جو بر اساس کد افتصادی">
+                            <x-backend.form.input name="economicID" :value="old('economicID')"/>
+                        </x-backend.form.form-group>
+                    </div>
                     <div class="col-lg-12">
                         <x-backend.form.form-group title="نوع تاریخ">
-                            <x-backend.form.radio-inline>
+{{--                            <x-backend.form.radio-inline>--}}
                                 <x-backend.form.radio title=" تاریخ ایجاد" name="date_type" value="created_at"/>
                                 <x-backend.form.radio title=" تاریخ اخرین بروزرسانی" name="date_type"
                                                       value="updated_at"/>
-                            </x-backend.form.radio-inline>
+{{--                            </x-backend.form.radio-inline>--}}
                         </x-backend.form.form-group>
                     </div>
 
@@ -31,13 +59,12 @@
             </div>
         </div>
 
-
         <x-slot name="footer">
             <button type="submit" class="btn btn-primary mr-2">
                 <x-backend.icon class="fa-file-search"/>
                 جست و جو
             </button>
-            <a class="btn btn-warning" href="{{ \App\Utilities\Url::admin('auth/user') }}">
+            <a class="btn btn-warning" href="{{ \App\Utilities\Url::admin('crm/invoice') }}">
                 <x-backend.icon class="fa-eraser"/>
                 پاک کن</a>
         </x-slot>

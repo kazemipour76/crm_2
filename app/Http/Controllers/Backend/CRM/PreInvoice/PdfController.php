15e74e63644a0981cc5fde53db7d9d5df90efe2a
kazemipour76 <ali.kazemipour@yahoo.com>
2022-02-11 08:50:12 +0330

    add term if for check status tax delete functions useless

--- app/Http/Controllers/Backend/CRM/PreInvoice/PdfController.php
+++ app/Http/Controllers/Backend/CRM/PreInvoice/PdfController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend\CRM\PreInvoice;
 use App\Http\Controllers\Controller;
 use App\Models\Auth\User;
 use App\Models\CRM\Customer;
+use App\Models\CRM\PreInvoice;
 use App\Utilities\Jdf;
 use App\Utilities\MessageBag;
 use Illuminate\Http\Request;
@@ -19,140 +20,46 @@ class PdfController extends Controller
     protected $modelName = 'مشتری';
     protected $viewFolder = 'CRM/PreInvoice/pdf';
 
-    public function filter()
-    {
-//        $model = $this->model::OrderBy('id')->withTrashed();
-        $model = $this->model::OrderBy('id');
-        $filter = request()->all();
-
-        if (!empty($filter['term'])) {
-            $model->search($filter['term']);
-        }
-
-        $date_type = '';
-        if (isset($filter['date_type'])) {
-            $date_type = $filter['date_type'];
-        }
-
-        if (isset($filter['date_from'])) {
-            $date = explode('/', $filter['date_from']);
-            $gdate = Jdf::jalali_to_gregorian($date[0], $date[1], $date[2], '-') . ' 00:00:00';
-            $model->where($date_type, '>=', $gdate);
-        }
-
-        if (isset($filter['date_to'])) {
-            $date = explode('/', $filter['date_to']);
-            $gdate = Jdf::jalali_to_gregorian($date[0], $date[1], $date[2], '-') . ' 23:59:59';
-            $model->where($date_type, '<=', $gdate);
-        }
-
-        return $model;
-    }
-
 
     public function index(Request $request)
     {
-        $models = $this->filter()->paginate(request('perPage', 5));
-        $old = \Request::flash($models);
-        $old = \Request::old($old);
-        $data = [
-            'models' => $models,
-            'old' => $old,
-        ];
-
-        return view("backend.{$this->viewFolder}.list", $data);
+        //
     }
 
-
-    public function destroy($id)
-    {
-        $this->deleteAction([$id]);
-        return redirect($this->returnDefault);
-    }
 
     public function create($id)
     {
         $preInvoiceDetails = $this->modelDetail::where('pre_invoice_id', $id)->get();
+        $data['details'] = $preInvoiceDetails;
         $model = $this->model::findOrFail($id);
         $totalSum = $model->totalPriceAll();
         $discount = $model->total_discount;
         $tax = $this->tax($totalSum);
-        $amountPayable = ($totalSum - $discount) + $tax;
-        $data['details'] = $preInvoiceDetails;
-        $data['tax'] = $tax;
         $data['totalSum'] = $totalSum;
-        $data['amountPayable'] = $amountPayable;
         $customers = Customer::all();
         $data['customers'] = $customers;
         $data['model'] = $model;
+        if ($model['type'] === PreInvoice::TYPE_RASMI) {
+            $amountPayable = ($totalSum - $discount) + $tax;
+        } else {
+            $amountPayable = $totalSum - $discount;
+            $tax = 0;
+        }
+        $data['tax'] = $tax;
+        $data['amountPayable'] = $amountPayable;
         return view("backend.$this->viewFolder", $data);
     }
+
     public function tax($totalSum)
     {
         $tax = ($totalSum * 9) / 100;
         return $tax;
     }
+
     public function store()
     {
-        $model = new $this->model;
-         request()->validate(Customer::getValidationCustomer());
-        $model->fill(request()->all());
-        if ($model->save()) {
-            MessageBag::push($this->modelName . ' با موفقیت ایجاد شد', MessageBag::TYPE_SUCCESS);
-            return redirect("{$this->returnDefault}/" . $model->id . '/edit');
-        } else {
-            MessageBag::push($this->modelName . ' ایجاد نشد لطفا مجددا تلاش فرمایید');
-            return redirect()->back();
-        }
-
-    }
-
-    public function edit($id)
-    {
-        $model = $this->model::findOrFail($id);
-        $data['model'] = $model;
-        return view("backend.{$this->viewFolder}.edit", $data);
-    }
-
-    public function update($id)
-    {
-        $model = $this->model::findOrFail($id);
-        request()->validate(Customer::getValidationCustomer(true, $id));
-        $model->fill(request()->all());
-        if ($model->save()) {
-            MessageBag::push($this->modelName . ' با موفقیت ویرایش شد', MessageBag::TYPE_SUCCESS);
-            return redirect()->back();
-        } else {
-            MessageBag::push('مجدد تلاش کنید ');
-            return redirect()->back();
-        }
+//
     }
 
 
-    /*
-     * ------------------------------- actions ------------------------------
-     */
-    public function actions(Request $request)
-    {
-        $ids = array_keys(request('checks', []));
-        $action = trim(strtolower(request('action', '')));
-
-        switch ($action) {
-            case 'delete':
-                $this->deleteAction($ids);
-                break;
-        }
-
-        return redirect()->back();
-    }
-
-    public function deleteAction($ids)
-    {
-        $count = count($ids);
-        if ($this->model::whereIn('id', $ids)->delete()) {
-            MessageBag::push("تعداد {$count} {$this->modelName} با موفقیت حذف شد" , MessageBag::TYPE_SUCCESS);
-        } else {
-            MessageBag::push("{$this->modelName}  حذف نشد لطفا مجددا تلاش فرمایید");
-        }
-    }
 }
